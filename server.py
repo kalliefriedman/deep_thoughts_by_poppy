218d312b82476498a3ba96b744a11c2644c89f02
vagrant <vagrant@vagrant.vm>
2017-05-18 22:56:18 +0000

    markov chains are generated as expected with 200 most recent tweets

--- server.py
+++ server.py
@@ -34,20 +34,70 @@ def make_new_tweet():
         access_token_secret=environ['TWITTER_ACCESS_TOKEN_SECRET'])
     print "connected"
     statuses = api.GetUserTimeline(user_id=None, screen_name=handle,
-                                   since_id=None, max_id=None, count=50,
+                                   since_id=None, max_id=None, count=200,
                                    include_rts=True, trim_user=False,
                                    exclude_replies=False)
     markov_input = ""
     if statuses:
         for status in statuses:
             markov_input += status.text
+            markov_input += " "
         print markov_input
+
+        # create a dictionary of key value pairs with the markov_input
+        markov_pairings = make_chains(markov_input)
+        
+        # create a new markov chain using the markov_pairings dictionary
+        newtweet = generate_text(markov_pairings)
+        print "newtweet: "+newtweet
+
+        # PriorTweets.save_tweet(handle, newtweet)
+
         return "hi"
     else:
         flash("There was a problem with the user you've identified. Please try a different user.")
         return redirect("/")
 
 #     then, generate a tweet, store that new tweet, send over that object to template
+
+
+def make_chains(text_string):
+    """Takes input text as string; returns _dictionary_ of markov chains."""
+    chains = {}
+    words = text_string.split()
+    for index in range(0, len(words)-2):
+        key = tuple([words[index], words[index+1]])
+        chains.setdefault(key, []).append(words[index+2]+" ")
+
+    # looping around the chain to create last two key/value pairs
+    chains.setdefault((words[-2], words[-1]), []).append(words[0]+" ")
+    chains.setdefault((words[-1], words[0]), []).append(words[1]+" ")
+    return chains
+
+
+def generate_text(chains):
+    """Takes dictionary of markov chains; returns random text."""
+
+    output_text = ""
+
+    # make key list and then choose the first key randomly
+    keys = chains.keys()
+    new_key = choice(keys)
+
+    # add the first tuple key to the output_text
+    output_text = output_text + new_key[0] + " " + new_key[1] + " "
+    # continues to add keys until text is too long or the key isn't in dict
+    while len(output_text) < 130:
+        print "in while loop!"
+        value = chains.get(new_key, None)
+        if value:
+            random_value = choice(value)
+        else:
+            break
+        output_text += (random_value+" ")
+        new_key = tuple([new_key[1], random_value])
+
+    return jsonify(output_text)
 
 
 @app.route('/get-past-tweets.json', methods=["GET"])
@@ -57,77 +107,6 @@ def get_prior_tweets():
 #     if user id exists in database, return that list of tweets to template to display
 
 
-# chains = {}  # dictionary for all the texts going through
-
-
-# def open_and_read_file(file_path):
-#     """Takes file path as string; returns text as string.
-#     Takes a string that is a file path, opens the file, and turns
-#     the file's contents as one string of text.
-#     """
-
-#     open_file = open(file_path)
-#     return open_file.read()
-
-
-# def make_chains(text_string, chains):
-#     """Takes input text as string; returns _dictionary_ of markov chains.
-#     A chain will be a key that consists of a tuple of (word1, word2)
-#     and the value would be a list of the word(s) that follow those two
-#     words in the input text.
-#     For example:
-#         >>> make_chains("hi there mary hi there juanita")
-#         {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
-#     """
-
-#     words = text_string.split()
-
-#     # Solution 2
-#     for index in range(0, len(words)-2):
-#         key = tuple([words[index], words[index+1]])
-#         chains.setdefault(key, []).append(words[index+2])
-#     chains.setdefault((words[-2], words[-1]), []).append(None)
-
-#     return chains
-
-
-# def make_text(chains):
-#     """Takes dictionary of markov chains; returns random text."""
-
-#     output_text = ""
-
-#     # made key list and then choise the first key random
-#     keys = chains.keys()
-#     new_key = choice(keys)
-
-#     # add the first tuple key to the output_text
-#     output_text = output_text + new_key[0] + " " + new_key[1]
-
-#     # from index 2 to end which is none, the code runs
-#     while True:
-#         random_value = choice(chains[new_key])
-#         if random_value is None:
-#             break
-#         output_text += " %s" % random_value
-#         new_key = tuple([new_key[1], random_value])
-
-#     return output_text
-
-
-# def combine_texts(argv):
-#     """Reads in arbitrary number of texts files and adds them to our dictionary."""
-
-#     # split the argv
-#     texts_to_add = sys.argv[1:]
-
-#     for text in texts_to_add:
-#         input_text = open_and_read_file(text)
-#         make_chains(input_text, chains)
-
-# # Produce random text
-# combine_texts(sys.argv)
-# random_text = make_text(chains)
-# print random_text
 
 
 if __name__ == "__main__":
